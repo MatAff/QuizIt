6f330385012fb5467aa144ca49551b61314ade1f
MatAff <mathijsaffourtit@gmail.com>
2020-04-26 21:27:02 -0600

    changing static location

--- quizitsite/quizitsite/settings.py
+++ quizitsite/quizitsite/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = "/var/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "var/static/")
+print(STATIC_ROOT)
 
 STATIC_URL = '/static/'
 
