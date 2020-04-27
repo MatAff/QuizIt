e2f8107a494bcf1cf6aafc4cf35c390be0c7d790
MatAff <mathijsaffourtit@gmail.com>
2020-04-26 21:16:59 -0600

    add collect static folder

--- quizitsite/quizitsite/settings.py
+++ quizitsite/quizitsite/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = "/var/static/"
+
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/basic/'
