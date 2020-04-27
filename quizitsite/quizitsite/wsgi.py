6a521089d3d7e5695cfd1dbd36969862b50dba15
MatAff <mathijsaffourtit@gmail.com>
2020-04-27 00:16:37 +0000

    update wsgi.py

--- quizitsite/quizitsite/wsgi.py
+++ quizitsite/quizitsite/wsgi.py
@@ -6,11 +6,15 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
-
 import os
+import sys
+
+sys.path.append('/home/bitnami/apps/django/django_projects/QuizIt/quizitsite')
+
+os.environ.setdefault("PYTHON_EGG_CACHE", "/home/bitnami/apps/django/django_projects/QuizIt/quizitsite/egg_cache")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quizitsite.settings")
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quizitsite.settings')
-
 application = get_wsgi_application()
+
