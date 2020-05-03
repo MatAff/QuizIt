f42726a0aaef79692a8269eb387f2cb1f2936429
MatAff <mathijsaffourtit@gmail.com>
2020-05-02 21:30:41 -0600

    Item selection refactor

--- quizitsite/quizit/learn.py
+++ quizitsite/quizit/learn.py
@@ -7,61 +7,26 @@ import pandas as pd
 
 class Learn(object):
 
-    def get_recent_items(self, response_df, n):
-        if response_df.shape[0] == 0:
-            return []
-        response_df = response_df.sort_values(by='ts', ascending=False)
-        return response_df['key'][0:n]
+    know_thresh_low = 0.7
+    know_thresh_high = 0.9
+    recent_n = 5
+    run_discount = 0.75
 
-    # TODO: refactor due to duplicate code in:
-    # compute_item_mean, compute_item_mean_running, add_mu, add_w_mu
-    def compute_item_mean(self, key, sorted_response_df):
-        item_responses = sorted_response_df[sorted_response_df.key==key]
-        if len(item_responses.index) == 0 : return 0.0 # handle no data
-        mu = item_responses.correct.mean()
-        mu = np.round(mu, 5)
-        return mu
-
-    def compute_item_mean_running(self, key, sorted_response_df, r):
-        item_responses = sorted_response_df[sorted_response_df.key==key]
-        if len(item_responses.index) == 0 : return 0.0 # handle no data
-        resp_int = item_responses.correct.astype('int').values
-        mu_r = reduce(lambda a, b: a * (1-r) + b * r, resp_int[::-1])
-        return mu_r
+    def get_recent_items(self, sorted_response_df, n):
+        n = min(len(sorted_response_df.index), n)
+        recent_items = sorted_response_df['key'][0:n]
+        return recent_items
 
     def order_retainer(self, s):
         small_addition = np.array(range(len(s), 0, -1)) / 10**6
         return s + small_addition
-    
-    def compute_item_count(self, key, sorted_response_df):
-        item_responses = sorted_response_df[sorted_response_df.key==key]
-        return len(item_responses.index)
-    
-    def add_count(self, item_df, response_df):
-        item_count = lambda k: self.compute_item_count(k, response_df)
-        item_df['n'] = item_df.key.apply(item_count)
-        return item_df
 
-    def add_mu(self, item_df, response_df):
-        item_mean = lambda k: self.compute_item_mean(k, response_df)
-        item_df['mu'] = item_df.key.apply(item_mean)
-        item_df['mu'] = self.order_retainer(item_df['mu'])
-        return item_df
-
-    def add_w_mu(self, item_df, response_df, r):
-        run_mean = lambda k: self.compute_item_mean_running(k, response_df, r)
-        item_df['mu_run'] = item_df.key.apply(run_mean)
-        item_df['mu_run'] = self.order_retainer(item_df['mu_run'])
-        return item_df
-
-    def get_chunk(self, df, col, val, n, db=False):
+    def get_chunk(self, df, col, val, n):
         ser = df[col]
         ind = ser[ser==val].index[0]
         pos = df.index.get_loc(ind)
         start_pos = max(0, (pos - n))
         end_pos = (pos + n + 1)
-        if db:
-            print(pos, start_pos, end_pos)
         return df.iloc[start_pos:end_pos]
 
     def compute_near_mu(self, k, item_df, n):
@@ -77,12 +42,50 @@ class Learn(object):
         item_df['mu_near'] = self.order_retainer(item_df['mu_near'])
         return item_df
 
+    def compute_item_stats(self, key, sorted_response_df, r):
+        item_responses = sorted_response_df[sorted_response_df.key==key]
+        n = len(item_responses.index)
+        if n == 0 : 
+            mu = 0.0
+            mu_run = 0.0
+            return {'n':n, 'mu': mu, 'mu_run': mu_run}
+        mu = np.round(item_responses.correct.mean(), 5)
+        resp = item_responses.correct.values
+        mu_run = reduce(lambda a, b: a * (1-r) + b * r, resp[::-1])
+        return {'n':n, 'mu': mu, 'mu_run': mu_run}
+
+    def add_item_stats(self, item_df, response_df):
+
+        # sort responses
+        response_df = response_df.sort_values(by='ts', ascending=False)
+        
+        # determine recent items
+        recent_items = self.get_recent_items(response_df, n=self.recent_n)
+        item_df['recent'] = item_df.key.isin(recent_items)
+
+        # compute items stats
+        r = self.run_discount
+        get_item_stats = lambda k: self.compute_item_stats(k, response_df, r)
+        item_stats = item_df.key.apply(get_item_stats)
+        item_df = pd.concat([item_df, item_stats.apply(pd.Series)], axis=1)
+
+        # compute near stats
+        item_df = self.add_near_mu(item_df, 5)
+
+        # combine
+        item_df['prob'] = item_df['mu_run']
+        fill = item_df.n == 0
+        item_df.loc[fill,'prob'] = item_df.loc[fill, 'mu_near']
+
+        return item_df
+
     def simple(self, item_df, response_df):
         
         all_items = item_df
 
         # vary know threshold
-        know_thresh = random() * 0.2 + 0.7 # 0.7-0.9
+        rand_range = lambda low, high: random() * (high - low) + low
+        know_thresh = rand_range(self.know_thresh_low, self.know_thresh_high)
         print(f'current know threshold: {know_thresh}')
 
         # if reponses is empty return random item
@@ -90,30 +93,10 @@ class Learn(object):
             random_int = randint(0, 10)
             return item_df.iloc[random_int, :]
 
-        # determine recent items
-        recent_items = self.get_recent_items(response_df, n=5)
-        item_df['recent'] = item_df.key.isin(recent_items)
+        # add item statistics
+        item_df = self.add_item_stats(item_df, response_df)
 
-        # compute current probabiliy
-        response_df = response_df.sort_values(by='ts', ascending=False)
-
-        # item count
-        item_df = self.add_count(item_df, response_df)
-
-        # simple mean
-        item_df = self.add_mu(item_df, response_df)
-        
-        # discounted mean
-        item_df = self.add_w_mu(item_df, response_df, r=0.75)
-
-        # add near mu
-        item_df = self.add_near_mu(item_df, 5)
-
-        # combine
-        item_df['prob'] = item_df['mu_run']
-        item_df.loc[item_df.n == 0,'prob'] = item_df.loc[item_df.n ==0, 'mu_near']
-
-        # sometime pick random known
+        # sometimes pick random known
         pick_known_prob = 0.15
         if random() < pick_known_prob:
             item_df = item_df[item_df.prob >= know_thresh]
@@ -126,7 +109,7 @@ class Learn(object):
         # pick item
         item_df = item_df[item_df.prob < know_thresh]
         item_df = item_df.sort_values('prob', ascending=False)
-        item_df = item_df[(item_df.key.isin(recent_items))==False]
+        item_df = item_df[item_df.recent==False]
         item_row = item_df.iloc[0, :]
 
         # debug (print item around chosen item)
@@ -135,3 +118,39 @@ class Learn(object):
         print(chunk)
 
         return item_row
+
+    # # compute_item_mean, compute_item_mean_running, add_mu, add_w_mu
+    # def compute_item_mean(self, key, sorted_response_df):
+    #     item_responses = sorted_response_df[sorted_response_df.key==key]
+    #     if len(item_responses.index) == 0 : return 0.0 # handle no data
+    #     mu = item_responses.correct.mean()
+    #     mu = np.round(mu, 5)
+    #     return mu
+
+    # def compute_item_mean_running(self, key, sorted_response_df, r):
+    #     item_responses = sorted_response_df[sorted_response_df.key==key]
+    #     if len(item_responses.index) == 0 : return 0.0 # handle no data
+    #     resp_int = item_responses.correct.astype('int').values
+    #     mu_r = reduce(lambda a, b: a * (1-r) + b * r, resp_int[::-1])
+    #     return mu_r
+    
+    # def compute_item_count(self, key, sorted_response_df):
+    #     item_responses = sorted_response_df[sorted_response_df.key==key]
+    #     return len(item_responses.index)
+    
+    # def add_count(self, item_df, response_df):
+    #     item_count = lambda k: self.compute_item_count(k, response_df)
+    #     item_df['n'] = item_df.key.apply(item_count)
+    #     return item_df
+
+    # def add_mu(self, item_df, response_df):
+    #     item_mean = lambda k: self.compute_item_mean(k, response_df)
+    #     item_df['mu'] = item_df.key.apply(item_mean)
+    #     item_df['mu'] = self.order_retainer(item_df['mu'])
+    #     return item_df
+
+    # def add_w_mu(self, item_df, response_df, r):
+    #     run_mean = lambda k: self.compute_item_mean_running(k, response_df, r)
+    #     item_df['mu_run'] = item_df.key.apply(run_mean)
+    #     item_df['mu_run'] = self.order_retainer(item_df['mu_run'])
+    #     return item_df
