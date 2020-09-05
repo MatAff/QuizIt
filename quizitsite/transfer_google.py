6f799e650d00d49aede6370154615518e3078463
MatAff <mathijsaffourtit@gmail.com>
2020-09-05 08:17:06 -0600

    transfer tags from duplicate keys

--- quizitsite/transfer_google.py
+++ quizitsite/transfer_google.py
@@ -36,6 +36,13 @@ topic = 'spanish'
 # topic = 'french'
 
 
+def duplicates_join_drop(df, dup_col, join_col):
+    tag_df = df.groupby(dup_col).agg(lambda r: ";".join(r[join_col]))
+    df.loc[:, join_col] = tag_df.loc[df[dup_col], join_col].values
+    df = df[df[dup_col].duplicated() == False]
+    return df
+
+
 def transfer_google():
 
     # get sheet content
@@ -48,7 +55,8 @@ def transfer_google():
     df['key'] = df.iloc[:, 0] + "|" + df.iloc[:, 1]
     df['tag'] = df['tag'].fillna('')
     df['alternatives'] = df['alternatives'].fillna('')
-    df = df[df.key.duplicated() == False]
+    df = duplicates_join_drop(df, 'key', 'tag')
+    # df = df[df.key.duplicated() == False]
     df = df.reset_index()
     print(df.head(10))
 
@@ -66,3 +74,18 @@ def transfer_google():
                     alts=row['alternatives'])
         print(item.question)
         item.save()
+
+
+def test_duplicates_join_drop():
+    df_in = pd.DataFrame({
+        "key": ['a', 'a', 'b'],
+        "tag": ["0", "2", "3"]
+        })
+    df_out = pd.DataFrame({
+        "key": ['a', 'b'],
+        "tag": ["0;2", "3"]
+        })
+    assert duplicates_join_drop(df_in, 'key', 'tag').reset_index(drop=True).equals(df_out)
+
+
+# test_duplicates_join_drop()
